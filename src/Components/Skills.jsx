7cf15a94ce48f3312f4bb90046d288a126b2f953
Md.Naimul Hasan <alnoman047@gmail.com>
2026-09-02 18:23:04 +0600

    added skills section

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -3,6 +3,7 @@ import icon from '../assets/icon.png'
 import icon1 from '../assets/icon1.png'
 import icon2 from '../assets/icon2.png'
 import icon3 from '../assets/icon3.png'
+import hand from '../assets/hand.png'
 
 const Skills = () => {
     const skills = [
@@ -39,7 +40,56 @@ const Skills = () => {
         <div
         className='h-96 w-96 bg-linear-to-br from-[#0268b0] to-blue-500 blur-2xl opacity-40 rounded-full'>
         </div>
-        <motion.div>
+        <motion.div
+        variants={containerVariants}
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: false}}
+        className='container mx-auto px-6 relative z-10'>
+           <div className=' flex- flex-col lg:flex-row items-center justify-between gap-12'>
+            {/* Images */}
+            <motion.div
+            variants={itemVariants}
+            className='lg:w-1/2 flex justify-center relative'>
+                <div className='relative'>
+                    <motion.img
+                    src={hand}
+                    alt='Image'
+                    className='w-full max-w-md relative z-10'
+                    whileHover={{ scale: 1.05}}
+                    transition={{ duration: 0.3}}>
+                    </motion.img>
+                {/* Icons */}
+                {skills.map((skill, index) => {
+                    const positions = [
+                        {top: '5%', left: '20%'},
+                        {top: '5%', left: '23%'},
+                        {bottom: '90%', left: '30%'},
+                        {bottom: '5%', left: '34%'},
+                    ];
+                    return (
+                        <motion.div
+                        key={skill.name}
+                        className='absolute z-20'
+                        style={positions[index]}
+                        animate={{
+                            y: [0, -10, 0],
+                            rotate: [0, 5, 0]
+                        }}
+                        transition={{
+                            duration: 3,
+                            repeat: Infinity,
+                            delay: index * 0.5,
+                            ease: 'easeInOut'
+                        }}>
+
+                        </motion.div>
+                    )
+                })}
+                </div>
+
+            </motion.div>
+            </div> 
 
         </motion.div>
         </div>
